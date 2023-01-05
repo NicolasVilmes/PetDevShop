d12790b63563ef256b3eb18a1e4de2ae70a2d256
Nicolas Vilmes <nicolasvilmes@outlook.com>
2023-01-04 21:43:26 -0300

    Defining Routes and Controllers

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './Routes/index'
 
 dotenv.config();
 
@@ -13,6 +14,10 @@ server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../Public')));
 
-//Routes
+server.use(mainRoutes);
+
+server.use((req, res) => {
+  res.send('Página não encontrada!');
+});
 
 server.listen(process.env.PORT);
