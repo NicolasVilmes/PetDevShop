6317d0b4de51350ced91effd9fef65d2e45880cd
Nicolas Vilmes <nicolasvilmes@outlook.com>
2023-01-04 22:23:35 -0300

    cofig routes

--- src/controller/pageController.ts
+++ src/controller/pageController.ts
@@ -1,17 +1,38 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-  res.render('Pages/main');
+  res.render('Pages/main', {
+    banner: {
+      title: 'Todos os animais',
+      background: 'allanimals.jpg'
+    }
+  });
 }
 
 export const dogs = (req: Request, res: Response) => {
-
+  res.render('Pages/main', {
+    banner: {
+      title: 'Cachorros',
+      background: 'banner_dog.jpg'
+    }
+  });
 }
 
-export const cats = (req: Request, res: Response) => {
 
+export const cats = (req: Request, res: Response) => {
+  res.render('Pages/main', {
+    banner: {
+      title: 'Gatos',
+      background: 'banner_cat.jpg'
+    }
+  });
 }
 
 export const fishes = (req: Request, res: Response) => {
-
+  res.render('Pages/main', {
+    banner: {
+      title: 'Peixes',
+      background: 'banner_fish.jpg'
+    }
+  });
 }
